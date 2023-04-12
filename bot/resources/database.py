bf549855d04665e598ad00ab62d6689e1e943c75
AlmadelX <almadelx8@gmail.com>
2023-04-12 10:30:12 +0300

    Database try/except, regex fix

--- bot/resources/database.py
+++ bot/resources/database.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy.ext.declarative as dec
+from bot.resources.logging import logger
 
 DATABASE_FILE = 'resources/database.db'
 
 SqlAlchemyBase = dec.declarative_base()
 
-engine = create_engine(f'sqlite:///{DATABASE_FILE}')
-factory = sessionmaker(bind=engine)
-db_session = factory()
+try:
+    engine = create_engine(f'sqlite:///{DATABASE_FILE}')
+    factory = sessionmaker(bind=engine)
+    db_session = factory()
+except Exception:
+    logger.critical('Database not found')
+    exit(-1)
